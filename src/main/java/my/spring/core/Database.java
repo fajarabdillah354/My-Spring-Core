b817f90f8b12d75b6678424212cb6503bc578299
unknown <fajardillah25@gmail.com>
2024-09-21 22:47:35 +0700

    Upload

---
+++ src/main/java/my/spring/core/Database.java
@@ -0,0 +1,20 @@
+package my.spring.core;
+
+import javax.xml.crypto.Data;
+
+public class Database {
+
+
+    private static Database database;
+
+    public static Database getInstance(){
+        if (database == null){
+            database = new Database();
+        }
+        return database;
+    }
+
+    private Database(){
+
+    }
+}
