798b52669d79cfe5db3a4d40b599152b8cb641ad
unknown <fajardillah25@gmail.com>
2024-09-29 20:46:35 +0700

    Upload

---
+++ src/main/java/my/spring/core/HelloWorldConfiguration.java
@@ -0,0 +1,11 @@
+package my.spring.core;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+    public static void main(String[] args) {
+
+    }
+
+}
