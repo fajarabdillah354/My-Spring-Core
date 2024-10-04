9f16604d7f853c7babb9a52df243177b01234c7f
unknown <fajardillah25@gmail.com>
2024-10-04 23:55:26 +0700

    Upload

---
+++ src/main/java/my/spring/core/PrimaryConfiguration.java
@@ -0,0 +1,24 @@
+package my.spring.core;
+
+import my.spring.core.data.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class PrimaryConfiguration {
+
+    @Primary//
+    @Bean
+    public Foo foo1(){
+        return new Foo();
+    }
+
+
+    @Bean
+    public Foo foo2(){
+        return new Foo();
+    }
+
+
+}
